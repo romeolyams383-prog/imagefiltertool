6ce74f42d18915f57f4dfc5d0e8872ce6f0750ad
Romeo Lyams <romeolyams383@gmail.com>
2026-09-18 11:20:08 +0100

    Initial commit: Spring Boot project setup

---
+++ imagefiltertool/src/main/java/com/romeo/imagefiltertool/ImagefiltertoolApplication.java
@@ -0,0 +1,13 @@
+package com.romeo.imagefiltertool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagefiltertoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImagefiltertoolApplication.class, args);
+	}
+
+}
---
+++ imagefiltertool/src/test/java/com/romeo/imagefiltertool/ImagefiltertoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romeo.imagefiltertool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagefiltertoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
